fd97ca8e53a90064e6b97f57c9e71a2bcf24a4ed
HeseltineBTutu <heseltinetutu@gmail.com>
2022-12-08 01:14:35 -0800

    task 6 done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio>
+/**
+ *main -Entry point
+ *Descritption: prints with proper grammar, but the outcome is a piece of art
+ *Return:Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+	return (0);
+}
