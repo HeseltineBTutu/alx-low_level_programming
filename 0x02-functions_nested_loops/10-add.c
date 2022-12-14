b5b66777b96ca0bebff904af09a11b2a656f197b
HeseltineBTutu <heseltinetutu@gmail.com>
2022-12-14 11:59:48 -0800

    description aded

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,8 @@
 /**
  * add- entry point
  *
- * Description: a function that adds two integers and returns the result.
+ * @num1: The first integer to be added
+ * @num2: The second integer to be added
  *
  * Return: The results of the addition
  *
