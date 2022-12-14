bd51a28d54c9c398c2df4156988833d724dbec03
HeseltineBTutu <heseltinetutu@gmail.com>
2022-12-14 11:54:56 -0800

    task 0-10 completed

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add- entry point
+ *
+ * Description: a function that adds two integers and returns the result.
+ *
+ * Return: The results of the addition
+ *
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
