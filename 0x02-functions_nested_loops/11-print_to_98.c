f36b6ea7b64435c191b4b2b878fbb9be6679a229
HeseltineBTutu <heseltinetutu@gmail.com>
2022-12-15 00:49:32 -0800

    error fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,28 +13,21 @@
  */
 void print_to_98(int n)
 {
-		int i;
+	int i;
 
-		for (i = n; i <= 98; i++)
+	if (n < 98)
+	{
+		for (i = n; i < 98; i++)
 		{
-			printf("%d", i);
-
-			if (i != 98)
-			{
-				printf(", ");
-			}
-			else 
-			{
-				if (n > 98)
-				{
-					for (i = n; i >= 98; i--)
-						printf("%d", i);
-					if (i != 98)
-					{
-						printf(", ");
-					}
-			}
+			printf("%d, ", i);
 		}
-		printf("\n");
-}
+	}
+	else
+	{
+		for (i = n; i > 98; i--)
+		{
+			printf("%d, ", i);
+		}
+	}
+	printf("98");
 }
