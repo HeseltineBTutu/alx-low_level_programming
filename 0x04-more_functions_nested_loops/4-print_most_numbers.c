6e337f2c89dc21ac6960164e1a56b7b26feb264c
HeseltineBTutu <heseltinetutu@gmail.com>
2022-12-16 10:33:29 -0800

    task 0-10 complete

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print- print the numbers from 0-9
+ *
+ * @2: to be skiped
+ * @4: to be skiped
+ *
+ */
+void print_most_numbers(void)
+{
+	int i, num2, num4;
+
+	num2 = 2;
+	num4 = 4;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != num2 && i != num4)
+		{
+			_putchar(i + '0')
+		}
+	}
+	_putchar('\n');
+}
