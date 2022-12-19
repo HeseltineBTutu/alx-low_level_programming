de97da101a953c0cfd5c540d6f0ae5fae6723625
HeseltineBTutu <heseltinetutu@gmail.com>
2022-12-19 07:45:31 -0800

    task 0 -11 complete

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts- prints a string
+ *
+ * Description: a function that print a string.
+ *
+ * @str: string to be printed
+ *
+ * Return: none
+ */
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
