d23381f6aaa8c4887d3f3a2f7e0c41aca8b89687
HeseltineBTutu <heseltinetutu@gmail.com>
2022-12-19 17:59:21 -0800

    task 0 - 11 complete

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,9 +17,9 @@ int _atoi(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == '-')
-			sign = -1;
+			sign *= -1;
 		else if (s[i] >= '0' && s[i] <= '9')
-			result = result * 10 + s[i] - '\0';
+			result = (result * 10) + (s[i] - '\0');
 		else if (result > 0)
 			break;
 	}
