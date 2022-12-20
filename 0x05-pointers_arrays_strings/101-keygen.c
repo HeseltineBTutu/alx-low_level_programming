a1ff95b727c279062f1c0b5cca2d5fc064552407
HeseltineBTutu <heseltinetutu@gmail.com>
2022-12-19 18:17:49 -0800

    all task done

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 8
+/**main - Generate a random password
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	srand(time(NUL));
+
+	int i;
+
+	for (i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		printf("%c", 'a' + rand() % 26);
+	}
+	printf("\n");
+	return (0);
+}
