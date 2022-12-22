b9c0f9e2b1442cad49521ca20e1d713ce864b470
HeseltineBTutu <heseltinetutu@gmail.com>
2022-12-22 02:04:30 -0800

    task 0 - 7 complete

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,20 +1,22 @@
 #include "main.h"
 /**
- * _strncpy: a function that copies a string
+ * _strncat: a function that concatenates two strings
  *
  * @dest: destination
  *
- * @n: bytes
+ * @src: source
+ *
+ * @n: number of bytes
  *
  * Return: dest
  */
-char *_strncpy(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
+	int dest_len = strlen(dest);
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for (; i < n; i++)
-		dest[i] = '\0';
+		dest[dest_len + 1] = src[i];
+	dest[dest_len + i] = '\0';
 	return (dest);
 }
