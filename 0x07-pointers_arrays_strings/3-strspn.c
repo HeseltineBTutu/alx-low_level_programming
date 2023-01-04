4e3fbfd37094a171659c9b2071fed01d3ab9859f
HeseltineBTutu <heseltinetutu@gmail.com>
2023-01-04 04:17:59 -0800

    task 0 - 3 complete

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,8 +13,7 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int count = 0;
 
-	while (*s && strchr(accept, *s++)){
-			count++;
-			}
-			return (count);
+	while (*s && strchr(accept, *s++))
+		count++;
+	return (count);
 }
