ad713683519d85f770a88ae5597296c2573290a5
HeseltineBTutu <heseltinetutu@gmail.com>
2023-01-04 02:03:09 -0800

    initial commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * _memset- entry point
  *
@@ -19,5 +18,5 @@ char *_memset(char *s, char b, unsigned int n)
 	{
 		s[i] = b;
 	}
-	return s;
+	return (s);
 }
