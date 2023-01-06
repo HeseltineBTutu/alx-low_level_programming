057823d5cbf9432dd6d68db48991a0c6fc5430f9
HeseltineBTutu <heseltinetutu@gmail.com>
2023-01-06 04:26:01 -0800

    betty coding style fix

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _pow_recursion- function that returns the value of x raised to the power of y.
+ * _pow_recursion- function that returns the value of x.
  *
  * @x: The number to be raised.
  * @y: The power.
