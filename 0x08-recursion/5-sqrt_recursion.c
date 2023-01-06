027e7028b832f8f0da029b2b0a461e665ec70cbf
HeseltineBTutu <heseltinetutu@gmail.com>
2023-01-06 06:24:01 -0800

    mixed declaration fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,16 +1,12 @@
 #include "main.h"
 /**
- * _sqrt_recursion- main function
+ * _sqrt_- returns squareroot
  *
  * @n: the number.
+ * @i: value to be tested
  *
  * Return: returns the natural sqauare root of a number.
- */
-int _sqrt_recursion(int n)
-{
-	return (_sqrt(n - 1));
-}
-			
+ */			
 int _sqrt(int n, int i)
 	if (n < 0)
 	{
@@ -20,9 +16,20 @@ int _sqrt(int n, int i)
 	{
 		return (-1);
 	}
-	if (i * i == n)
+	if ((i * i) == n)
 	{
 		return (i);
 	}
-	return _sqrt(n, i + 1);
-}				
+	return (_sqrt(n, i + 1));
+}
+/**
+ * _sqrt_recursion- returns squarevroot of a natural number.
+ *
+ * @n: number to find sqrt
+ *
+ * Return: sqrt, (-1) notba natural number.
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
