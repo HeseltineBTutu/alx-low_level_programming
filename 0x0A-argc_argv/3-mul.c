474e6e54e131d5f31594d7b4b3767f42f88891c8
HeseltineBTutu <heseltinetutu@gmail.com>
2023-01-09 21:52:51 -0800

    task 3- 4 complete

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,6 +14,7 @@ int main(int argc, char *argv[])
 {
 	int num1;
 	int num2;
+	int result;
 
 	if(argc !=2)
 	{
@@ -24,7 +25,7 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 
-	int result = num1 * num2;
+	result = num1 * num2;
 	printf("%d\n", result);
 	return (0);
 }
