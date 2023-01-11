b279ce3f4a06b8321e3f4d2ee5e1f19bd92f1289
HeseltineBTutu <heseltinetutu@gmail.com>
2023-01-11 05:44:05 -0800

    initial declaration fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,13 +11,14 @@
 int **alloc_grid(int width, int height)
 {
 	int **grid;
+	int i;
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
 	grid = (int **)calloc(height, sizeof(int *));
-	for (int i = 0; i < height; i++)
+	for (i = 0; i < height; i++)
 	{
 		grid[i] = (int *)calloc(width, sizeof(int));
 	}
