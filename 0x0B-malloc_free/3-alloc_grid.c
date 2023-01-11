05c6511e5aaf9f2939c4d9ad68888c1c463733f4
HeseltineBTutu <heseltinetutu@gmail.com>
2023-01-11 05:34:22 -0800

    task 3 complete

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,17 +27,17 @@ int **alloc_grid(int width, int height)
 		grid[i] = (int *)malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
-			for (int j = 0; j < i; j++)
+			for (j = 0; j < i; j++)
 			{
-				free(grid[j];
+				free(grid[j]);
 			}
 			free(grid);
 			return (NULL);
 		}
 	}
-	for (int i = 0; i < height; i++)
+	for (i = 0; i < height; i++)
 	{
-	for (int j = 0; j < width; j++)
+	for (j = 0; j < width; j++)
 	{
 	grid[i][j] = 0;
 	}
