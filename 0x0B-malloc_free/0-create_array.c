eb576e26af4fd7da0a1b98e683eb4fa2fdf11215
HeseltineBTutu <heseltinetutu@gmail.com>
2023-01-11 02:28:33 -0800

    added header file for malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,13 +12,14 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
+	char *arr;
 
 	if (size == 0)
 		return (NULL);
-	char *arr = (char *)malloc(sizeof(char) * size);
+	arr = (char *)malloc(sizeof(char) * size);
 
 	if (arr == NULL)
-		return (NUll);
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
