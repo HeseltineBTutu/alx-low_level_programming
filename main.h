408cd51eaf08d862328ea45e7b73871b649549f6
HeseltineBTutu <heseltinetutu@gmail.com>
2023-01-28 05:21:25 -0800

    Revert "Initial implementation of _printf function, it only supports c, s and % conversion specifiers."
    This reverts commit cd8207df0e8c9d76ca52a355aea6e1530b816a9b.
    
     Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	deleted:    .main.h.swp
    	deleted:    _printf.c
    	deleted:    a.out
    	deleted:    main.c
    	deleted:    main.h

--- main.h
+++
@@ -1,9 +0,0 @@
-#ifndef PRINTF_H
-#define PRINTF_H
-int _printf(const char *format, ...);
-#include <stdarg.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#endif
