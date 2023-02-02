85ff7fd76d551ff14d23236105434c490d3e8055
HeseltineBTutu <heseltinetutu@gmail.com>
2023-02-02 06:45:43 -0800

    added 4-free_listint

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint - a function that remove space allocated to each node.
+ * @head: a pointer to the head of the list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head !=NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
