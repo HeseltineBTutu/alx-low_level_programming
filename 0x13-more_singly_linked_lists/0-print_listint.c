8793c55a746a79f29cbab5051fa863e86707726c
HeseltineBTutu <heseltinetutu@gmail.com>
2023-02-02 04:07:25 -0800

    Added Error handler

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,9 +10,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		printf(" %d\n", h->n);
+		if (h->n == NULL)
+			print("Error\n");
+		else
+			printf(" %d\n", h->n);
 		h = h->next;
 		count++;
 	}
