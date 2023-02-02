e6bc097e1c2ff3c942104791a8e3d8ca32f08bb1
HeseltineBTutu <heseltinetutu@gmail.com>
2023-02-02 06:49:43 -0800

    added 4-free_listint

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,10 +8,10 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-	while (head !=NULL)
+	while (head)
 	{
-		temp = head;
-		head = head->next;
-		free(temp);
+		temp = head->next;
+		free(head);
+		head = temp;
 	}
 }
