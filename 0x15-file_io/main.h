da58e0908eb7c0236dda573abac83db684f3ea4e
HeseltineBTutu <heseltinetutu@gmail.com>
2023-02-09 02:14:20 -0800

    1added 100-elf_header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,10 +4,29 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <errno.h>
+#include <sys/stat.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 #define BUFSIZE 1024
 #include <string.h>
+#define EI_NIDENT 16
+typedef struct {
+  unsigned char e_ident[EI_NIDENT];
+  unsigned short e_type;
+  unsigned short e_machine;
+  unsigned int e_version;
+  unsigned int e_entry;
+  unsigned int e_phoff;
+  unsigned int e_shoff;
+  unsigned int e_flags;
+  unsigned short e_ehsize;
+  unsigned short e_phentsize;
+  unsigned short e_phnum;
+  unsigned short e_shentsize;
+  unsigned short e_shnum;
+  unsigned short e_shstrndx;
+} Elf32_Ehdr;
 #endif
