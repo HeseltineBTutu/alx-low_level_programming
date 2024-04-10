c5304d6cc1405efe9cd11a8427276b3809ef3872
HeseltineBTutu <heseltinetutu@gmail.com>
2024-04-10 10:19:49 -0700

    added  @ symbol before each parameter name

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -3,9 +3,9 @@
 /**
  * linear_search - searches for a value in an array of integers using
  * the Linear search algorithm
- * array: a pointer to the first element of the array to search in
- * size: the number of elements in array
- * value:  the value to search for
+ * @array: a pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value:  the value to search for
  *
  * return: value on sucess or, -1 on failure
  */
