487fee3b61fc1bff249532d50ec68ade1c4bb5f4
HeseltineBTutu <heseltinetutu@gmail.com>
2024-04-13 00:51:12 -0700

    added check for NULL in array

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -16,6 +16,9 @@ int jump_search(int *array, size_t size, int value)
 	size_t step = sqrt(size);
 	size_t prev = 0;
 
+	if (array == NULL)
+		return (-1);
+
 	while (array[MIN(step, size) -  1] < value)
 	{
 		printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
